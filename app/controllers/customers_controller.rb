3a9524779916751d5bae8734d18cacef734c8505
YamaguchiHitoshi <hitoshi.gaia.galaxy@gmail.com>
2018-05-03 11:29:52 +0900

    登録処理作成

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,17 @@
 class CustomersController < ApplicationController
+  # before_save:
+
   def index
   end
 
   def new
+    @customer = Customer.new #フォーム作成のためのオブジェクト
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    @customer.save
+    redirect_to @customer
   end
 
   def edit
@@ -15,8 +21,19 @@ class CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def destroy
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email
+    )
   end
 end
