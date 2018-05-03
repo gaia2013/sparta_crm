39b4af268f9a141c5b5306861a5b2dffded12de0
YamaguchiHitoshi <hitoshi.gaia.galaxy@gmail.com>
2018-05-03 16:17:10 +0900

    検索機能追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,7 +3,9 @@ class CustomersController < ApplicationController
   # before_action :set_company, only: [:new, :create, :edit, :update]
 
   def index
-    @customers = Customer.page(params[:page])
+    # @customers = Customer.page(params[:page])
+    @q = Customer.includes(:post, :company).ransack(params[:q])
+    @customers = @q.result.page(params[:page])
   end
 
   def new
